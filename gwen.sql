9ef4109c94845b866fde4a70e7fae59fd006ff95
20110702 <20110702@student.hcmute.edu.vn>
2022-10-11 01:12:38 +0700

    aaa

--- gwen.sql
+++ gwen.sql
@@ -1,4 +1,4 @@
--- Create a new database called 'Gwen'
+﻿-- Create a new database called 'Gwen'
 -- Connect to the 'master' database to run this snippet
 USE master
 GO
@@ -23,9 +23,9 @@ CREATE TABLE [dbo].[customer]
     [Id] INT IDENTITY(0,1) PRIMARY KEY, -- Primary Key column
     [f_name] NVARCHAR(50) NOT NULL,
     [address] NVARCHAR(100) NOT NULL,
-    [phone] VARCHAR(10),
-    [email] VARCHAR(50),
-    [password] VARCHAR(30)
+    [phone] VARCHAR(10) NOT NULL,
+    [email] VARCHAR(50) NOT NULL,
+    [password] VARCHAR(30) NOT NULL
     -- Specify more columns here
 );
 GO
@@ -55,12 +55,68 @@ CREATE TABLE [dbo].[contact]
 (
     [user_id] INT,
     [f_name] NVARCHAR(50) NOT NULL,
-    [phone] VARCHAR(10),
-    [email] VARCHAR(50),
-    [note] NVARCHAR(100),
+    [phone] VARCHAR(10) NOT NULL,
+    [email] VARCHAR(50) NOT NULL,
+    [note] NVARCHAR(100) NOT NULL,
     [date_created] DATETIME DEFAULT CURRENT_TIMESTAMP 
     -- Specify more columns here
     FOREIGN key (user_id) REFERENCES [dbo].[user] (Id)
+);
+GO
+
+-- Create a new table called '[product]' in schema '[dbo]'
+-- Drop the table if it already exists
+IF OBJECT_ID('[dbo].[product]', 'U') IS NOT NULL
+DROP TABLE [dbo].[product]
+GO
+-- Create the table in the specified schema
+CREATE TABLE [dbo].[product]
+(
+    [Id] INT IDENTITY(0,1) PRIMARY KEY, -- primary key column
+    [name] NVARCHAR(50) NOT NULL,
+    [description] NVARCHAR(50) NOT NULL,
+    [type] NVARCHAR(10) NOT NULL,
+    [price] FLOAT NOT NULL,
+    [quantity] INT NOT NULL,
+    [date_created] DATETIME DEFAULT CURRENT_TIMESTAMP,
+    -- specify more columns here
+);
+GO
+
+-- Create a new table called '[order]' in schema '[dbo]'
+-- Drop the table if it already exists
+IF OBJECT_ID('[dbo].[order]', 'U') IS NOT NULL
+DROP TABLE [dbo].[order]
+GO
+-- Create the table in the specified schema
+CREATE TABLE [dbo].[order]
+(
+    [Id] INT IDENTITY(0,1) PRIMARY KEY, -- primary key column
+    [customer_id] INT NOT NULL,
+    [user_id] INT NOT NULL,
+    [date_created] DATETIME DEFAULT CURRENT_TIMESTAMP,
+    [note] NVARCHAR(100) NOT NULL,
+    -- specify more columns here
+    FOREIGN key (customer_id) REFERENCES [dbo].[customer] (Id),
+    FOREIGN key (user_id) REFERENCES [dbo].[user] (Id)
+);
+GO
+
+-- Create a new table called '[order_detail]' in schema '[dbo]'
+-- Drop the table if it already exists
+IF OBJECT_ID('[dbo].[order_detail]', 'U') IS NOT NULL
+DROP TABLE [dbo].[order_detail]
+GO
+-- Create the table in the specified schema
+CREATE TABLE [dbo].[order_detail]
+(
+    [Id] INT , -- primary key column
+    [order_id] INT NOT NULL,
+    [product_id] INT NOT NULL,
+    [quantity] INT NOT NULL,
+    -- specify more columns here
+    FOREIGN key (order_id) REFERENCES [dbo].[order] (Id),
+    FOREIGN key (product_id) REFERENCES [dbo].[product] (Id)
 );
 GO
 
